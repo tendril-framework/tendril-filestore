cde2f6fd193337e4a98dc833818679fa760885d7
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-12-14 19:15:11 +0530

    Expand database sessions to include fs accesses

--- src/tendril/filestore/actual.py
+++ src/tendril/filestore/actual.py
@@ -13,6 +13,8 @@ from tendril.filestore.db.controller import register_stored_file
 from tendril.filestore.db.controller import change_file_bucket
 from tendril.filestore.db.controller import delete_stored_file
 
+from tendril.utils.db import with_db
+from tendril.utils.db import get_session
 from tendril.utils import log
 logger = log.get_logger(__name__, log.DEFAULT)
 
@@ -41,13 +43,14 @@ class FilestoreBucket(FilestoreBucketBase):
         b = register_bucket(name=self.name)
         self._id = b.id
 
-    def upload(self, file, user, overwrite=False):
+    @with_db
+    def upload(self, file, user, overwrite=False, session=None):
         filename = file.filename
 
         if self._fs.exists(filename):
             if not overwrite:
                 raise FileExistsError(f'{filename} already exists in the {self.name} bucket. Delete it first.')
-            owner = get_storedfile_owner(filename, self._id)
+            owner = get_storedfile_owner(filename, self._id, session=session)
             if not self._allow_overwrite and owner.puid != user:
                 raise FileExistsError(f'{filename} already exists in the {self.name} bucket and owned by someone else.')
             logger.warning(f"Overwriting file {filename} in bucket {self.name}.")
@@ -78,11 +81,12 @@ class FilestoreBucket(FilestoreBucketBase):
         fileinfo = {'props': {'size': info.size, 'created': created, 'modified': modified},
                     'hash': {'sha256': sha256hash.hexdigest()}}
 
-        sf = register_stored_file(filename, self._id, user, fileinfo)
+        sf = register_stored_file(filename, self._id, user, fileinfo, session=session)
 
         return sf
 
-    def move(self, filename, target_bucket, user, overwrite=False):
+    @with_db
+    def move(self, filename, target_bucket, user, overwrite=False, session=None):
         if not self._fs.exists(filename):
             raise FileNotFoundError(f"Move of nonexisting file {filename} "
                                     f"from bucket {self.name} requested.")
@@ -90,30 +94,31 @@ class FilestoreBucket(FilestoreBucketBase):
         if target_bucket.fs.exists(filename):
             if not overwrite:
                 raise FileExistsError(f'{filename} already exists in the {target_bucket.name} bucket. Delete it first.')
-            owner = get_storedfile_owner(filename, target_bucket.id)
+            owner = get_storedfile_owner(filename, target_bucket.id, session=session)
             if not target_bucket._allow_overwrite and owner.puid != user:
                 raise FileExistsError(f'{filename} already exists in the {target_bucket.name} bucket and owned by someone else.')
             logger.warning(f"Overwriting file {filename} in bucket {target_bucket.name}.")
-            target_bucket.remove(filename)
+            target_bucket.delete(filename)
 
         logger.debug(f"Moving file {filename} from bucket {self.name} to {target_bucket.name}")
         move.move_file(self.fs, filename, target_bucket.fs, filename)
-        return change_file_bucket(filename, self.id, target_bucket.id, user)
+        return change_file_bucket(filename, self.id, target_bucket.id, user, session=session)
 
-    def delete(self, filename, user):
+    @with_db
+    def delete(self, filename, user, session=None):
         if not self._fs.exists(filename):
             raise FileNotFoundError(f"Delete of nonexisting file {filename} "
                                     f"from bucket {self.name} requested.")
 
         if not self._allow_delete:
-            owner = get_storedfile_owner(filename, self._id)
+            owner = get_storedfile_owner(filename, self._id, session=session)
             if owner.puid != user:
                 raise PermissionError(f"Deletion of file {filename} "
                                       f"not permitted from bucket {self.name}")
 
         logger.info(f"Deleting {filename} from bucket {self.name}")
         self.fs.remove(filename)
-        delete_stored_file(filename, self.id, user)
+        delete_stored_file(filename, self.id, user, session=session)
 
     def list(self):
         return self.fs.listdir('/')
@@ -124,9 +129,10 @@ class FilestoreBucket(FilestoreBucketBase):
                                   f"is not permitted")
         logger.warning(f"Purging all files from bucket {self.name}")
         for filename in self.list():
-            logger.info(f"Deleting file {filename} from bucket {self.name}")
-            self.fs.remove(filename)
-            delete_stored_file(filename, self.id, user)
+            with get_session() as session:
+                logger.info(f"Deleting file {filename} from bucket {self.name}")
+                self.fs.remove(filename)
+                delete_stored_file(filename, self.id, user, session=session)
 
     def __repr__(self):
         return "<FilestoreBucket {} at {}>".format(self.name, self.uri)
