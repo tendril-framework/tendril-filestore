a0260d2d55c54900ed233ad9f774cad09bfe1db4
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-03-13 11:50:07 +0530

    Disable fm scopes when fm is disabled.

--- src/tendril/authz/scopes/filestore.py
+++ src/tendril/authz/scopes/filestore.py
@@ -1,6 +1,12 @@
 
 
-scopes = {
-    'file_management:common': "Standard file management API access",
-    'file_management:admin': "Administrative file management API access"
-}
+from tendril.config import FILESTORE_ENABLED
+
+
+if FILESTORE_ENABLED:
+    scopes = {
+        'file_management:common': "Standard file management API access",
+        'file_management:admin': "Administrative file management API access"
+    }
+else:
+    scopes = {}
