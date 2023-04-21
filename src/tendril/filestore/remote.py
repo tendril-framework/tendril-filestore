c1925f97155268b67527d8c77ae6f8928f8f7b0d
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-04-21 13:39:25 +0530

    Async client integration to the class.

--- src/tendril/filestore/remote.py
+++ src/tendril/filestore/remote.py
@@ -1,8 +1,8 @@
 
 
-from urllib.parse import urljoin
 from tendril.authn.client import IntramuralAuthenticator
 from tendril.utils.www import async_client
+from tendril.utils.www import with_async_client_cl
 
 from tendril.config import FILESTORE_REMOTE_AUDIENCE
 from tendril.config import FILESTORE_REMOTE_CLIENT_ID
@@ -25,26 +25,39 @@ async def get_remote_bucket_list(remote_uri):
     async with async_client(base_url=remote_uri, auth=_authenticator) as client:
         response = await client.get('/v1/filestore/buckets')
         return response.json()["available_buckets"]
+    # return['incoming', 'cdn', 'outgoing']
 
 
 class FilestoreBucketRemote(FilestoreBucketBase):
+    def _async_http_client_args(self):
+        return {
+            'base_url': self.uri,
+            'auth': _authenticator
+        }
+
     async def upload(self, file, user, overwrite=False):
-        pass
+        raise NotImplementedError
 
     async def move(self, filename, target_bucket, user, overwrite=False):
-        pass
+        raise NotImplementedError
 
     async def delete(self, filename, user):
-        pass
+        raise NotImplementedError
 
     async def list(self):
-        pass
+        raise NotImplementedError
+
+    @with_async_client_cl()
+    async def list_info(self, include_owner=False, filenames=None, client=None):
+        response = await client.post(f'/v1/filestore/{self.name}/ls',
+                                     params={'include_owner': include_owner})
+        return response.json()
 
     async def purge(self, user):
-        pass
+        raise NotImplementedError
 
     async def prune(self, user):
-        pass
+        raise NotImplementedError
 
     async def find(self, spec):
-        pass
+        raise NotImplementedError
